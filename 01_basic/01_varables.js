8d719e17807a19391558678a8b105ef60e596938
Anupam Kumar <102190695+annnuuupam@users.noreply.github.com>
2025-06-15 08:20:37 +0000

    firstProgram

--- 01_basic/01_varables.js
+++ 01_basic/01_varables.js
@@ -2,7 +2,8 @@ const accountId = 144553
 let accountEmail = "anuapmkumar3062@gmail.com"
 var accountPassword = "1234"
 accountCity = "Motihari"
+let accountState
 
 console.log(accountCity);
-console.table([accountId,accountEmail, accountPassword, accountCity])
+console.table([accountId,accountEmail, accountPassword, accountCity,accountState])
 
