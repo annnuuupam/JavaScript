e172211df5a36ad41fcb57fce7a3e8429afcb66e
Anupam Kumar <102190695+annnuuupam@users.noreply.github.com>
2025-06-15 08:16:58 +0000

    firstProgram

---
+++ 01_basic/01_varables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "anuapmkumar3062@gmail.com"
+var accountPassword = "1234"
+accountCity = "Motihari"
+
+console.log(accountCity);
+console.table([accountId,accountEmail, accountPassword, accountCity])
+
